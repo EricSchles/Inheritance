ce9e5769765b92e846ab73ec0c9c72ba3293d07d
Eric Schles <ericschles@gmail.com>
2014-08-04 21:28:27 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+    public class Animal
+    {
+        public virtual void Greet()
+        {
+            Console.WriteLine("Hello, I'm some sort of animal!");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Greet()
+        {
+            base.Greet();
+            Console.WriteLine("Yes I am - a dog!");
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Greet();
+            Dog dog = new Dog();
+            dog.Greet();
+            Console.ReadKey();
+
+        }
+    }
+}
